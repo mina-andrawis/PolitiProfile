4c38942785e2456e6ffc539980260207d76682b2
Mina Andrawis <mi.andrawis@gmail.com>
2024-10-05 18:59:43 -0400

    Removed CSS Module, replaced with tailwind

--- politiprofile/components/layout.js
+++ politiprofile/components/layout.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/layout.module.css";
-import utilStyles from "../styles/utils.module.css";
 import NavBar from "./navigation/navbar";
 
 const name = "PolitiProfile";
@@ -10,14 +8,14 @@ export const siteTitle = "PolitiProfile";
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <div className="max-w-4xl px-6 my-12 mx-auto">
       <Head>
         <meta
           name="description"
           content="Connect with politicians and candidates that align with your ideals. Compare candidates for elections and discover insights on key political issues."
         />
       </Head>
-      <header className={styles.header}>
+      <header className="flex flex-col items-center">
         <h1 className="text-5xl text-center font-bold font-mono mb-3">{name}</h1>
       </header>
 
@@ -25,7 +23,7 @@ export default function Layout({ children, home }) {
       
       <main>{children}</main>
       {!home && (
-        <div className={styles.backToHome}>
+        <div className="mt-12">
           <Link href="/">← Back to home</Link>
         </div>
       )}
