6acf7a8244e1a32c7a27bbfac7036008a6fda71d
Mina Andrawis <mi.andrawis@gmail.com>
2024-10-07 15:22:29 -0400

    More mobile optimizing

--- politiprofile/pages/account.js
+++ politiprofile/pages/account.js
@@ -13,7 +13,7 @@ const Account = () => {
 
   const { logout, loading } = useAuth(); // Get the current user and logout function
   const { user } = useAuthState(); // Get the current user
-  const {userDetails} = useUserDetails();
+  const { userDetails } = useUserDetails();
 
   if (loading) {
     return (
@@ -28,30 +28,32 @@ const Account = () => {
       <Head>
         <title>My Account</title>
       </Head>
-      <div className="mx-auto w-full p-6 text-left">
-        <h1 className="mb-4 text-4xl font-bold">My Account</h1>
+      <div className="mx-auto w-full p-4 sm:p-6 text-left">
+        <h1 className="mb-4 text-3xl sm:text-4xl font-bold">My Account</h1>
 
         {user ? (
-          <div className="flex">
-            <div className="w-1/5 py-4 pr-4 min-w-fit">
-              <p className="mb-4">Welcome, {userDetails?.email}</p>
+          <div className="flex flex-col sm:flex-row">
+            {/* Sidebar with buttons */}
+            <div className="w-full sm:w-1/5 py-4 pr-4 sm:min-w-fit">
+              <p className="mb-4 text-lg sm:text-base">Welcome, {userDetails?.email}</p>
 
               <button
-                className="p-2 mb-3 w-full bg-secondary text-white rounded-md hover:bg-blue-600"
+                className="p-2 mb-3 w-full bg-secondary text-white rounded-md hover:bg-secondaryHover"
                 onClick={() => setAccountSelection('profile-information')}
               >
                 Profile Information
               </button>
 
               <button
-                className="p-2 mb-3 w-full bg-secondary text-white rounded-md hover:bg-blue-600"
+                className="p-2 mb-3 w-full bg-secondary text-white rounded-md hover:bg-secondaryHover"
                 onClick={() => setAccountSelection('profile-topics')}
               >
                 Your Important Topics
               </button>
             </div>
 
-            <div className="w-4/5 rounded-lg bg-sand">
+            {/* Main Content Area */}
+            <div className="w-full sm:w-4/5 rounded-lg bg-sand mt-4 sm:mt-0">
               <AccountSelection selection={accountSelection} />
             </div>
           </div>
