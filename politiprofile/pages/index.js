02327e4ad7b078ba7775b4e9846efd5109dd4d1a
Mina Andrawis <mi.andrawis@gmail.com>
2025-07-07 16:25:33 -0400

    Resolve immediate rerouting on index.js, fixing SEO issues

--- politiprofile/pages/index.js
+++ politiprofile/pages/index.js
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 // This file redirects the user to the featured fighters page when they visit the root URL (default in Next.js).
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/featured-fighters",
+      permanent: false, // Set to true if it's a permanent redirect (301)
+    },
+  };
+}
+
 export default function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/featured-fighters");
-  }, [router]);
-
-  return null; // Nothing to render while redirecting
+  return null;
 }
