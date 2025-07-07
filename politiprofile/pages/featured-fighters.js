1f357c97e158b3f1161ccbc70f5a57e5100d3ad1
Mina Andrawis <mi.andrawis@gmail.com>
2025-07-07 16:47:51 -0400

    Update featured-fighters.js

--- politiprofile/pages/featured-fighters.js
+++ politiprofile/pages/featured-fighters.js
@@ -8,7 +8,7 @@ import Sidebar from "../components/fighters/sidebar";
 
 export default function Home({ initialFighters, initialTotalPages }) {
   const { page, setPage, handleNextPage, handlePrevPage } = usePagination();
-  const { fighters, loading, error, totalPages } = useFighters('', page, 30);
+  const { fighters, loading, error, totalPages } = useFighters('', page, 4);
 
   const [sidebarOpen, setSidebarOpen] = useState(false); // mobile toggle
 
